e223c575b8d1b5405dffebc02276aa6a9d8534b7
qubilyan <151318276+qubilyan@users.noreply.github.com>
2022-10-22 12:30:07 +0000

    KVM: s390: Zero out current VMDB of STSI before including level3 data.

--- exampleToCopyIntoApps/ofxBackgroundTest/001FromOpencvExample/src/testApp.h
+++ exampleToCopyIntoApps/ofxBackgroundTest/001FromOpencvExample/src/testApp.h
@@ -14,4 +14,14 @@ class testApp : public ofBaseApp{
 		
 		void keyPressed(int key);
 		void keyReleased(int key);
-		void mouseMoved(i
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);		
+
+	ofVideoGrabber 		vidGrabber;			//to get a live video image to analyse
+	ofxCvColorImage		colorImg;			//a place to save the live video frame
+	ofxBackground		backgroundAddon;	//the addon that allow
