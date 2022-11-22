0f30dcd8219197a9eed3995d5dc5dd7647db1f44
qubilyan <151318276+qubilyan@users.noreply.github.com>
2022-11-22 20:16:14 +0000

    Fix #30

--- src/ofxBackground.cpp
+++ src/ofxBackground.cpp
@@ -1,4 +1,25 @@
 /*
  *  ofxBackground.cpp
  *
- *  Created by Joel Gethin Lewi
+ *  Created by Joel Gethin Lewis on 15/03/2010.
+ *
+ */
+
+#include "ofxBackground.h"
+
+ofxBackgroundLearningCompleteEvent ofxBackground::onLearningComplete;
+
+	//--------------------------------------------------------------------------------
+ofxBackground::ofxBackground() {
+    _width = 0;
+    _height = 0;
+	
+	nChannels = CHANNELS;
+	imageLen = 0;
+	
+    ImaskAVG = 0, ImaskAVGCC = 0;
+    ImaskCodeBook = 0, ImaskCodeBookCC = 0;
+	
+	maxMod[0] = 3;  //Set color thresholds to default values
+	minMod[0] = 10;
+	maxMod[
