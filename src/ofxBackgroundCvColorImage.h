c3ba12a7928e2b7f5ef1e9abb2972cdab51df74d
qubilyan <151318276+qubilyan@users.noreply.github.com>
2023-03-12 22:56:08 +0000

    update dossier

--- src/ofxBackgroundCvColorImage.h
+++ src/ofxBackgroundCvColorImage.h
@@ -17,4 +17,16 @@ public:
 	virtual void  convertRgbToYuv(){
 			cvCvtColor( cvImage, cvImageTemp, CV_BGR2YCrCb);
 			swapTemp();
-			flagImageCha
+			flagImageChanged();
+	}; //jgl add
+	
+public: //need all the assignment, see Chaper 13 "Using inheritance and dynamic binding" "Accelerated C++"
+	
+    virtual void  operator -= ( float value );
+    virtual void  operator += ( float value ); 
+	
+    virtual void  operator = ( unsigned char* _pixels );
+    virtual void  operator = ( const ofxCvGrayscaleImage& mom );
+    virtual void  operator = ( const ofxCvColorImage& mom );
+    virtual void  operator = ( const ofxCvFloatImage& mom );
+  
